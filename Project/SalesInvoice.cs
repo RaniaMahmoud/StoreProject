4690a45643c44240f9aa8f7877b2a894f8fdaa26
Rania Mahmoud <rosyblack88@yahoo.com>
2022-02-26 16:06:09 +0200

    Supplier&SalesInvoiceTast

--- Project/SalesInvoice.cs
+++ Project/SalesInvoice.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project
 {
-    public class SalesInvoice:Invoice
+    public class SalesInvoice
     {
-        public List<ProductInfo> ProductsInfo  { get; set; }
+        [Key]
+        public int ID { set; get; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime InvoiceDate { get; set; }
+        [Required]
+        public double Total { get; set; }
+        public virtual Customer Customer { set; get; }
+        //public List<ProductInfo> ProductsInfo  { get; set; }
 
-        public static int ID = 0;
-        public string Customer { get; set; }
+        //public static int ID = 0;
+        //public string Customer { get; set; }
         public SalesInvoice()
         {
-            ID++;
-            ProductsInfo = new List<ProductInfo>();
+            //ID++;
+            InvoiceDate = DateTime.Now;
+            //ProductsInfo = new List<ProductInfo>();
         }
     }
 }
