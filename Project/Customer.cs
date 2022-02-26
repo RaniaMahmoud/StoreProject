b1a42ffe049c32a656541375ea3927cf2ad402d8
sleem <sleemshafa6@gmail.com>
2022-02-26 11:29:11 +0200

    Do Class Customer & Class Admin

--- Project/Customer.cs
+++ Project/Customer.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project
 {
-    public class Customer:Person
+    public class Customer
     {
-        #region Constructor
-        public Customer()
-        {
-
-        }
-        public Customer(string Name, string Phone, string Address) : base(Name, Phone, Address)
-        {
-
-        }
-        #endregion
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(50),Required]
+        public string Name { get; set; }
+        [MaxLength(11),Required]
+        public string Phone { get; set; }
+        [MaxLength,Required]
+        public string Address { get; set; }
+        public virtual SalesInvoice  SalesInvoice { get; set; }
     }
 }
